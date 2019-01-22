213e4988285ce50cc3c62a8df41d312a2ea80b1d
umairx97 <umair.bajwa@hotmail.com>
2019-01-22 20:50:56 +0500

    Music Project start

--- src/index.js
+++ src/index.js
@@ -2,21 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import Routes from "./routes";
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
-
-import reducers from './reducers'; 
 
 const App = () => {
-  return <div>hello</div>;
+  return (
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  );
 };
 
-
-const createStoreWithMiddleware = applyMiddleware()(createStore)
-
-ReactDOM.render(
-  <Provider store = {createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
--- src/reducers/movies_reducer.js
+++
@@ -1,5 +0,0 @@
-//1_Action returns an object 
-
-
-
-//2_Reducer finds a match
