be11160443d73b4257c6ab8c14e3173d5e129f56
umairx97 <umair.bajwa@hotmail.com>
2019-01-22 19:41:13 +0500

    Adding redux

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import Routes from './routes'
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import Routes from "./routes";
+import { Provider } from "react-redux";
 
 const App = () => {
-    return(
-        <BrowserRouter>
-            <Routes/>
-        </BrowserRouter>
-    )
-}
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
+  return <div>hello</div>;
+};
+ReactDOM.render(
+  <Provider>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
