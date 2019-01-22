7ca68a0e28d57fa48d2b8478c1c75dc64da45256
umairx97 <umair.bajwa@hotmail.com>
2019-01-22 19:42:21 +0500

    Adding redux

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import Routes from "./routes";
 import { Provider } from "react-redux";
+import {createStore, applyMiddelware} from 'redux'; 
 
 const App = () => {
   return <div>hello</div>;
