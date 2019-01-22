0e5d6cd1d3ee2263c14bb590271d96df77a7d461
umairx97 <umair.bajwa@hotmail.com>
2019-01-22 19:48:45 +0500

    adding redux

--- src/index.js
+++ src/index.js
@@ -3,13 +3,17 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import Routes from "./routes";
 import { Provider } from "react-redux";
-import {createStore, applyMiddelware} from 'redux'; 
+import { createStore, applyMiddleware } from "redux";
 
 const App = () => {
   return <div>hello</div>;
 };
+
+
+const createStoreWithMiddleware = applyMiddleware()(createStore)
+
 ReactDOM.render(
-  <Provider>
+  <Provider store = {createStoreWithMiddleware()}>
     <App />
   </Provider>,
   document.getElementById("root")
